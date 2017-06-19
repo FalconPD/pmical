9146f4c2e89794dd3fa61e5551c8d2edc4226075
Ryan <Ryan.Tolboom@monroe.k12.nj.us>
2017-06-19 07:27:17 -0400

    Continued updates to delete member

--- pmical.php
+++ pmical.php
@@ -8,6 +8,9 @@ class pmical
   const DTFORMAT = 'Ymd\THis\Z';
   const HEADERBYTES = 30; //How many bytes to check at the beginning of an ics file
                           //This covers the BEGIN and VERSION statement
+  const TAILBYTES = 15; //How many bytes we cut off the end of the file before we append
+                        //this removes the END:VCALENDAR\r\n bytes
+  const BLOCKSIZE = 1024; 
 
   //This is a template for an empty calendar
   //According to the RFC a calendar needs at least
@@ -30,14 +33,20 @@ END:VEVENT\r
 END:VCALENDAR\r\n
 EOT;
 
+  const BEGINVEVENT = "BEGIN:VEVENT\r\n";
+  const ENDVEVENT = "END:VEVENT\r\n";
+
   private $fileName;
   private $lines;
   private $config;
+  private $printBlocks;
 
   public function __construct($name)
   {
     //Load the config
     $this->config = include("config.php");
+
+    $this->printBlocks = false;
 
     //Lookup the location of the calendar file by name
     if (! array_key_exists($name, $this->config['calendars']))
@@ -59,8 +68,7 @@ EOT;
         if (! $handle)
           die("Unable to open new calendar file: $this->fileName\n");
         $size = strlen($emptyCal);
-        if (fwrite($handle, $emptyCal, $size) != $size)
-          die("Unable to write $size bytes to new calendar file $this->fileName\n");
+        if (fwrite($handle, $emptyCal, $size) != $size) die("Unable to write $size bytes to new calendar file $this->fileName\n");
         fclose($handle);
 
         break;
@@ -122,60 +130,88 @@ EOT;
     $this->lines = array();
   }
 
-  //Deletes an event in the lines array by UID and returns TRUE.
-  //If the event isn't there it returns FALSE.
-  private function deleteFromLines($uid)
+  //Copy things from one file handle to another in blocks. This prevents us from
+  //having to allocate huge arrays for large files
+  private function copy($handle1, $handle2, $bytes)
   {
-    //Go line by line and find our UID. Make a note of the start
-    //and end indexes for the event
-    $uidLine = "UID:$uid";
+    while ($bytes)
+    {
+      if ($bytes >= self::BLOCKSIZE)
+        $read = self::BLOCKSIZE;
+      else
+        $read = $bytes;
+      $block = fread($handle1, $read);
+      if (! $block)
+        die("Error reading during copy.\n");
+      if ($this->printBlocks)
+        print($block);
+      $write = fwrite($handle2, $block);
+      if ($write != $read)
+        die("Error writing during copy.\n");
+      $bytes -= $write;
+    }
+  }
+
+  public function delete($uid)
+  {
+    //Read through the file line by line and get offset for the start and end of
+    //the event
+    $original = fopen($this->fileName, "r");
+    $fileSize = filesize($this->fileName);
+    if (! $original)
+      die("Unable to open $this->fileName for reading\n");
     $eventStartIndex = 0;
     $eventEndIndex = 0;
     $eventUidIndex = 0;
-    foreach($this->lines as $index=>$line)
+    $uidLine = "UID:$uid\r\n";
+    while  ($line = fgets($original))
     {
-      switch ($line) {
-        case "BEGIN:VEVENT":
-          $eventStartIndex = $index;
+      switch ($line)
+      {
+        case self::BEGINVEVENT:
+          $eventStartIndex = ftell($original) - strlen(self::BEGINVEVENT);
           $eventEndIndex = 0;
           break;
-        case "END:VEVENT":
-          $eventEndIndex = $index;
+        case self::ENDVEVENT:
+          $eventEndIndex = ftell($original);
           break;
         case $uidLine:
-          $eventUidIndex = $index;
+          $eventUidIndex = ftell($original);
           break;
       }
       if ($eventStartIndex && $eventEndIndex && $eventUidIndex)
-      {
-        //Remove the lines associated with that UID
-        $lines = array_splice($this->lines, $eventStartIndex,
-                              $eventEndIndex - $eventStartIndex + 1);
-        return TRUE;
-      }
+        break;
     }
-    return FALSE;
-  }
 
-  //Reads the calendar file, deletes the event (if there), and writes
-  //the calendar file (if needed)
-  public function delete($uid)
-  {
-    $this->read();
-    if ($this->deleteFromLines($uid))
-      $this->write();
+    //If we found something copy the first part of the file to a temp file
+    //copy it to a temp file but skip the event we want to delete.
+    if ($eventUidIndex)
+    {
+      print("$eventStartIndex $eventEndIndex $eventUidIndex\n");
+      if (fseek($original, 0, SEEK_SET))
+        die("Unable to seek to beginning of $this->fileName\n");
+      $tmpfname = tempnam(sys_get_temp_dir(), 'FOO');
+      if (! $tmpfname)
+        die("Uanble to create temporary file name.\n");
+      $tmpFile = fopen(tmpfname, "w");
+      if (! $tmpFile)
+        die("Unable to open temporary calendar file.\n");
+      $this->copy($original, $tmpFile, $eventStartIndex);
+      if (fseek($original, $eventEndIndex, SEEK_SET))
+        die("Unable to seek to $eventEndIndex in $this->fileName\n");
+      $this->printBlocks = true;
+      $this->copy($original, $tmpFile, $fileSize - $eventEndIndex);
+      fclose($tmpFile);
+      if (! rename($this->fileName, $tmpfname))
+        die("Unable to rename $tmpfname to $this->fileName\n");
+    }
+    fclose($original);
   }
 
   public function save($uid, $start, $end, $description, $summary)
   {
     //Don't allow multiple events with the same UID
-    $this->deleteFromLines($uid);
-
-    //Load the calendar from the file
-    $this->read();
-    
-    //Remove the END:VCALENDAR from the end of the file
-    array_pop($this->lines);
+    $this->delete($uid);
 
     //Put all the datetimes in the correct format
     $dtstart = $start->format(self::DTFORMAT);
@@ -185,12 +221,6 @@ EOT;
     $search = array('%uid%', '%dtstamp%', '%dtstart%', '%dtend%', '%description%', '%summary%');
     $replace = array($uid, $this->getDtStamp(), $dtstart, $dtend, $description, $summary);
     $additionalLines = str_replace($search, $replace, $this->config['newEvent']);
-    
-    //add this event on the end
-    $this->lines = array_merge($this->lines, $additionalLines);
-
-    //Write the file again
-    $this->write();
   } 
 }
 
